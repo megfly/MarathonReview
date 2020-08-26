485b5025d7a8e7b7ae4eecf397054de6a0e0b5b1
megfly <mflynn333@gmail.com>
2020-08-25 18:42:14 -0700

    initial configuration of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :runs
+
+  root to: 'sessions#home'
+
+  #signup
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  #logging in
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users do 
+    resources :runs, only: [:index]
+  end 
+
+  resources :runs do 
+    resources :reviews, only: [:new, :edit, :index, :show]
+  end 
   resources :reviews
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
