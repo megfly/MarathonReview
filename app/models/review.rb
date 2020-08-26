e83ecee8459622de6db9ae37204e110aa5be96ff
megfly <mflynn333@gmail.com>
2020-08-25 17:53:53 -0700

    added a scope

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,19 +3,6 @@ class Review < ApplicationRecord
     belongs_to :run 
 
     validates :title, :review_race_name, :description, :rating, presence: true
-    
+
+    scope :highest_rating, -> { order(rating: :desc) }
 end
-
-
-create_table "reviews", force: :cascade do |t|
-    t.integer "run_id"
-    t.integer "user_id"
-    t.string "title"
-    t.string "review_race_name"
-    t.text "description"
-    t.integer "rating"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["run_id"], name: "index_reviews_on_run_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
