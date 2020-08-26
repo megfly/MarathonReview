a7989c803e346bb603b9f66a4694dc548cb7bdfa
megfly <mflynn333@gmail.com>
2020-08-26 14:03:13 -0700

    created the initial views for runs

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,24 +1,38 @@
 class RunsController < ApplicationController
 
     def index 
+        @runs = Run.all
     end 
 
     def show
+        @run = Run.find(params[:id])
     end 
 
     def new
+        @run = Run.new 
     end 
 
     def create
+        @run = Run.build(run_params)
+        if @run.save 
+            redirect_to run_path 
+        else 
+            render :new 
+        end 
     end 
 
     def edit 
+        @run = Run.find(params[:id])
     end
 
     def update 
+        @run = Run.find(params[:id])
+        @run.update(run_params)
+        redirect_to run_path 
     end 
 
     def destroy
+        @run = Run.find(params[:id])
     end
 
     private
