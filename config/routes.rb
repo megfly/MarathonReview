56838719a0958f2a984b8d0b2cb37ff7d9154c81
megfly <mflynn333@gmail.com>
2020-09-02 13:53:32 -0400

    added resource routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :edit, :index, :show]
   end 
   resources :reviews
+
+  resources :users 
   #gem or api, 3rd party login, bootstrap, submit a photo review a run, nested form run&review at same time
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
