527093f7bd3141a9ebda046bb383f334d8bf0f58
megfly <mflynn333@gmail.com>
2020-09-02 13:52:42 -0400

    modified create controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,10 @@ class UsersController < ApplicationController
     end 
 
     def create
-        @user = User.create(user_params)
+        @user = User.new(user_params)
         if @user.save 
             session[:user_id] = @user.id 
-            redirect_to user_path(@user) #user show page 
+            redirect_to @user #user show page 
         else 
             redirect_to '/signup'
         end 
