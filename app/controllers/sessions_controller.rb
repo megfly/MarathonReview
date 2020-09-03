182c74d612e3650c1444397e7f6a4d2f177c70c2
megfly <mflynn333@gmail.com>
2020-09-03 16:49:24 -0400

    added methods to use omniauth facebook login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -29,6 +29,13 @@ class SessionsController < ApplicationController
     def update 
     end 
 
+    def omniauth
+        @user = User.from_omniauth(auth)
+        @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+    end 
+
     def destroy
         session.clear 
         redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
     has_many :runs, through: :reviews
 
     validates :email, presence: true, uniqueness: true 
+
+    def self.find_or_create_from_auth_hash(auth)
+        where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+            user.provider = auth.provider
+            user.uid = auth.uid
+            user.email = auth.info.email
+            user.name = auth.info.name 
+            user.image = auth.info.image
+            user.save!
+    end
+
 end
