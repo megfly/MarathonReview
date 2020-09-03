06e7588885a8af3cc1d6fb17bfa4d6dc1a2d4c9c
megfly <mflynn333@gmail.com>
2020-09-03 16:22:09 -0400

    added private method: auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -33,4 +33,10 @@ class SessionsController < ApplicationController
         session.clear 
         redirect_to root_path
     end
+
+    private 
+
+    def auth 
+        request.env['omniauth.auth']
+    end
 end 
