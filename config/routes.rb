1a1355263516f5c47d62cee9c8f404b2b6268343
megfly <mflynn333@gmail.com>
2020-09-03 17:02:30 -0400

    changed omniauth callback route to go to sessions controller, omniauth method

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   #fb omniauth logging in
-  get '/auth/facebook/callback' => 'sessions#create'
+  get '/auth/facebook/callback' => 'sessions#omniauth'
 
   resources :runs do 
     resources :reviews, only: [:new, :edit, :index, :show]
