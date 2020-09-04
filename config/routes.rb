39e265964d6c7378fb3fd30a7a8f16e1783b37d8
megfly <mflynn333@gmail.com>
2020-09-04 14:39:39 -0400

    created create route for runs/:id/review

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   get '/auth/facebook/callback', to: 'sessions#omniauth'
 
   resources :runs do 
-    resources :reviews, only: [:new, :edit, :index, :show]
+    resources :reviews, only: [:new, :create, :edit, :index, :show]
   end 
   resources :reviews
 
