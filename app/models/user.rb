8bc9a57f30b8ffd8580f29d3b125357216ea02f1
megfly <mflynn333@gmail.com>
2020-09-08 12:25:25 -0400

    trying to setup omniaut

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,32 +6,10 @@ class User < ApplicationRecord
 
     validates :email, presence: true, uniqueness: true 
 
-    def self.create_with_omniauth(auth)
-
-        #The ActiveRecord method find_or_create_by will find the user if the user exists or 
-        #create a new user if the user does not exist. 
-        #Then it will set the user to a variable called user. 
-  
-        user = find_or_create_by(uid: auth[‘uid’], provider:  auth[‘provider’]) 
-        user.email = “#{auth[‘uid’]}@#{auth[‘provider’]}.com”
-        user.password = auth[‘uid’]
-        user.name = auth[‘info’][‘name’]
-
-        #If the user exists, return user
-        #Else, save new user in db and return user
-
-        if User.exists?(user)
-          user
-        else
-          user.save!
-          user
+    def self.from_omniauth(auth)
+      @user = User.find_or_create_by(email: auth["info"]["email"]) do |user|
+          user.password = SecureRandom.hex
         end
-      end
-
-      private 
-
-      def auth
-          request.env['omniauth.auth']
-      end
+      end 
 
 end
