4b195b80050ef0e1c12c5bff76fdf5bd8aa41717
megfly <mflynn333@gmail.com>
2020-09-08 18:35:04 -0400

    added belongs_to :user association

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,8 +1,7 @@
 class Run < ApplicationRecord
-
+    belongs_to :user #now we have @run.user
     has_many :reviews
     has_many :users, through: :reviews 
 
     validates :run_race_name, :terrain, :location_city, :location_state, :month, presence: true 
-
 end
