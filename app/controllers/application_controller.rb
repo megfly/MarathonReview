387edc1aec0292bb56f343363851234cfa893876
megfly <mflynn333@gmail.com>
2020-09-08 12:24:38 -0400

    trying to use flash messages but they arent working, also edited current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,11 @@ class ApplicationController < ActionController::Base
     end
 
     def current_user 
-        @current_user ||= User.find_by(id: session[:user_id])
+        if session[:user_id]
+              @current_user ||= User.find(session[:user_id])
+        else
+              @current_user = nil
+        end
     end 
 
     def user_signed_in?
