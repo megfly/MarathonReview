8769229651094c68cd82efee1d061892201e68e0
megfly <mflynn333@gmail.com>
2020-09-08 12:26:37 -0400

    trying to enable flash messages but they arent working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,10 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save 
             session[:user_id] = @user.id 
+            flash[:error] = "Your account has been created!"
             redirect_to @user #user show page 
         else 
+            flash[:error] = "Please try again!"
             redirect_to '/signup'
         end 
     end 
