a66fa150ab16408d1c2d35b89a417874b2fd1c8d
megfly <mflynn333@gmail.com>
2020-09-08 10:24:03 -0400

    created the self.create_with_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,26 @@ class User < ApplicationRecord
 
     validates :email, presence: true, uniqueness: true 
 
+    def self.create_with_omniauth(auth)
+
+        #The ActiveRecord method find_or_create_by will find the user if the user exists or 
+        #create a new user if the user does not exist. 
+        #Then it will set the user to a variable called user. 
+  
+        user = find_or_create_by(uid: auth[‘uid’], provider:  auth[‘provider’]) 
+        user.email = “#{auth[‘uid’]}@#{auth[‘provider’]}.com”
+        user.password = auth[‘uid’]
+        user.name = auth[‘info’][‘name’]
+
+        #If the user exists, return user
+        #Else, save new user in db and return user
+
+        if User.exists?(user)
+          user
+        else
+          user.save!
+          user
+        end
+      end
+
 end
