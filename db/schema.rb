addb4cb38117c65e940946fa5575beb8ad109401
megfly <mflynn333@gmail.com>
2020-09-08 18:33:39 -0400

    added user_id to runs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_135757) do
+ActiveRecord::Schema.define(version: 2020_09_08_222229) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "run_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_09_08_135757) do
     t.string "month"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +47,5 @@ ActiveRecord::Schema.define(version: 2020_09_08_135757) do
     t.string "provider"
   end
 
+  add_foreign_key "runs", "users"
 end
