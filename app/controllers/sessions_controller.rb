ed815e9a434f6039de5f489f7b8c7eff221ca359
megfly <mflynn333@gmail.com>
2020-09-09 11:20:11 -0400

    modified controller actions: create and omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,27 +13,32 @@ class SessionsController < ApplicationController
     end 
 
     def create
-        if @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        @user = User.find_by(email: params[:user][:email]) #params from login form 
+ 
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id #save the user id inside browser cookies and login
+                flash[:error] = "Login successful!"
+                redirect_to user_path(@user)
+            else 
+                flash[:error] = "Please try again!"
+                redirect_to '/login'
+            end 
+    end 
+
+    def omniauth
+        @user = User.from_omniauth(auth)
+            @user = User.find_or_create_by(uid: auth['uid']) do |u|
             u.name = auth['info']['name']
             u.email = auth['info']['email']
             u.image = auth['info']['image']
                 if @user.save 
                     session[:user_id] = @user.id 
                     redirect_to user_path(@user)
-        else 
-            @user = User.find_by(email: params[:user][:email]) #params from login form 
- 
-                if @user && @user.authenticate(params[:user][:password])
-                    session[:user_id] = @user.id #save the user id inside browser cookies and login
-                    flash[:error] = "Login successful!"
-                    redirect_to user_path(@user)
                 else 
                     flash[:error] = "Please try again!"
                     redirect_to '/login'
                 end 
             end 
-            end 
-        end
     end 
 
     #request.env
