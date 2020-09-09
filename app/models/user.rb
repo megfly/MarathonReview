0efcbb91e7b872277c51ad71e30dbfee443c334c
megfly <mflynn333@gmail.com>
2020-09-09 11:19:49 -0400

    modified self.omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,10 @@ class User < ApplicationRecord
 
     validates :email, presence: true, uniqueness: true 
 
-    # def self.from_omniauth(auth)
-    #   @user = User.find_or_create_by(email: auth["info"]["email"]) do |user|
-    #     #user.email = auth["info"][""]
-    #       user.password = SecureRandom.hex
-    #     end
-    #   end 
+    def self.from_omniauth(auth)
+      @user = User.find_or_create_by(email: auth["info"]["email"]) do |user|
+        user.password = SecureRandom.hex
+      end
+    end 
 
 end
