450c7f7d5def68d2e5f5cf5939deb82ed43497b9
megfly <mflynn333@gmail.com>
2020-09-11 13:19:43 -0400

    added accepts_nested_attributes_for :reviews

--- app/models/run.rb
+++ app/models/run.rb
@@ -2,6 +2,7 @@ class Run < ApplicationRecord
     belongs_to :user #now we have @run.user
     has_many :reviews
     has_many :users, through: :reviews 
+    accepts_nested_attributes_for :reviews
 
     validates :run_race_name, :terrain, :location_city, :location_state, :month, presence: true 
 end
