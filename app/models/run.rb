abda0447ac50c45f777cb7d3bb7e8c078a5af48c
megfly <mflynn333@gmail.com>
2020-09-11 14:20:13 -0400

    troubleshooting nested form validation error-  modified validation to:  has_many :reviews, inverse_of :run

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,6 +1,9 @@
 class Run < ApplicationRecord
     belongs_to :user #now we have @run.user
-    has_many :reviews
+    #https://github.com/rails/rails/issues/25198 
+    #https://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html#method-i-has_many
+    #i am confused where to put the inverse
+    has_many :reviews, inverse_of: :run
     has_many :users, through: :reviews 
     accepts_nested_attributes_for :reviews
 
